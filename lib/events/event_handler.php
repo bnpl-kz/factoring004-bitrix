79b989369b6a5dd733cfa7384b44d0b683a9688e
Mirlan Jelamanov <mirlan.jelamanov@gmail.com>
2022-02-11 14:47:34 +0600

    Disables payment when required options are empty

--- lib/events/event_handler.php
+++ lib/events/event_handler.php
@@ -3,13 +3,22 @@
 namespace Bnpl\Payment;
 
 use Bitrix\Main\HttpRequest;
+use Bitrix\Sale\BusinessValue;
 use Bitrix\Sale\Internals\BusinessValuePersonDomainTable;
 use Bitrix\Sale\Order;
 
 class EventHandler
 {
-    private const MIN_SUM = 6000;
-    private const MAX_SUM = 200000;
+    const MIN_SUM = 6000;
+    const MAX_SUM = 200000;
+    const REQUIRED_OPTIONS = [
+        'BNPL_PAYMENT_CONSUMER_KEY',
+        'BNPL_PAYMENT_CONSUMER_SECRET',
+        'BNPL_PAYMENT_API_HOST',
+        'BNPL_PAYMENT_PARTNER_NAME',
+        'BNPL_PAYMENT_PARTNER_CODE',
+        'BNPL_PAYMENT_POINT_CODE',
+    ];
 
     public static function hidePaySystem(
         Order $order,
@@ -20,6 +29,11 @@ class EventHandler
         array &$arDeliveryServiceAll,
         &$arPaySystemServiceAll
     ) {
+        if (!static::isRequiredOptionsFilled()) {
+            static::disablePaymentSystemIfEnabled($arPaySystemServiceAll);
+            return;
+        }
+
         if (!static::isIndividualPersonType($arUserResult['PERSON_TYPE_ID'])) {
             static::disablePaymentSystemIfEnabled($arPaySystemServiceAll);
         }
@@ -54,5 +68,19 @@ class EventHandler
         if ($index) {
             unset($paymentSystems[$index]);
         }
+    }
+
+    /**
+     * @return bool
+     */
+    private static function isRequiredOptionsFilled()
+    {
+        foreach (static::REQUIRED_OPTIONS as $option) {
+            if (!BusinessValue::get($option, 'bnpl.payment')) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
