a37df14d8f7bbef08a18b6819c0819dc94f8cba1
Mirlan Jelamanov <mirlan.jelamanov@gmail.com>
2022-02-18 12:39:44 +0600

    Adds js script to disable delivery buttons

---
+++ template/sale_order_view_disable_script.php
@@ -0,0 +1,7 @@
+<script>
+  document.addEventListener('DOMContentLoaded', () => {
+    BX.Sale.Admin.OrderShipment.prototype.initFieldChangeStatus = function() {
+      console.log('initFieldChangeStatus', 'Disabled by plugin');
+    };
+  });
+</script>
