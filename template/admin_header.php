12bf1c045bdc2e2ab88df481f7f81374699be473
Mirlan Jelamanov <mirlan.jelamanov@gmail.com>
2022-03-04 18:40:20 +0600

    Removes payment cancel buttons

--- template/admin_header.php
+++ template/admin_header.php
@@ -22,6 +22,10 @@ if (strpos($_SERVER['REQUEST_URI'], 'admin/sale_order_view.php') !== false) {
             require_once __DIR__ . '/sale_order_view_disable_script.php';
             return;
         }
+
+        if ($__order->isPaid()) {
+            require_once __DIR__ . '/disable_payment_status.php';
+        }
     } catch (ArgumentNullException $e) {
         // do nothing
         return;
@@ -52,4 +56,19 @@ if (strpos($_SERVER['REQUEST_URI'], 'admin/sale_order_view.php') !== false) {
 
     \Bitrix\Main\UI\Extension::load('ui.notification');
     require_once __DIR__ . '/sale_order_shipment_edit.php';
+} elseif (strpos($_SERVER['REQUEST_URI'], 'admin/sale_order_payment_edit.php') !== false) {
+    try {
+        $__order = Order::load($_GET['order_id']);
+
+        if ((int) $__order->getField('PAY_SYSTEM_ID') !== (int) Config::getPaySystemId()) {
+            return;
+        }
+
+        if ($__order->isPaid()) {
+            require_once __DIR__ . '/disable_payment_status.php';
+        }
+    } catch (ArgumentNullException $e) {
+        // do nothing
+        return;
+    }
 }
---
+++ template/disable_payment_status.php
@@ -0,0 +1,120 @@
+<script>
+  BX.Sale.Admin.OrderPayment.prototype.initPaidPopup = function () {
+    var generalStatusFields = BX.findChildrenByClassName(BX('PAYMENT_BLOCK_STATUS_INFO_' + this.index), 'not_paid', true);
+    var returnStatusFields = BX.findChildrenByClassName(BX('PAYMENT_BLOCK_STATUS_INFO_' + this.index), 'return', true);
+
+    var indexes = [this.index];
+    if (this.viewForm) indexes.push(this.index + '_SHORT');
+
+    var menu = [];
+
+    if (Object.keys(this.psToReturn).length > 0) {
+      menu.push(
+        {
+          'ID': 'RETURN',
+          'TEXT': BX.message('PAYMENT_PAID_RETURN'),
+          'ONCLICK': BX.proxy(function () {
+            if (this.viewForm) {
+              this.showReturnWindow('return');
+            } else {
+              if (BX('PAYMENT_PAID_' + indexes[k]))
+                BX('PAYMENT_PAID_' + indexes[k]).value = 'N';
+
+              var isReturnChanged = BX('PAYMENT_IS_RETURN_CHANGED_' + indexes[k]);
+              if (isReturnChanged) {
+                isReturnChanged.value = 'Y';
+              }
+
+              var obOperation = BX('OPERATION_ID_' + this.index);
+              if (obOperation)
+                obOperation.disabled = false;
+
+              var isReturn = BX('PAYMENT_IS_RETURN_' + indexes[k]);
+              if (isReturn)
+                isReturn.value = 'Y';
+
+              this.changeNotPaidStatus('NO');
+
+              for (var i in generalStatusFields) {
+                if (!generalStatusFields.hasOwnProperty(i))
+                  continue;
+                BX.style(generalStatusFields[i], 'display', 'table-row');
+              }
+              for (var i in returnStatusFields) {
+                if (!returnStatusFields.hasOwnProperty(i))
+                  continue;
+                BX.style(returnStatusFields[i], 'display', 'table-row');
+              }
+
+              BX.bind(BX('OPERATION_ID_' + this.index), 'change', function () {
+                var tr = BX.findParent(this, { tag: 'tr' });
+                if (tr) {
+                  var style = (this.value != 'Y') ? 'none' : 'table-row';
+                  BX.style(tr.nextElementSibling, 'display', style);
+                }
+              });
+            }
+          }, this),
+        },
+      );
+    }
+
+    if (!this.viewForm) {
+      menu.unshift(
+        {
+          'ID': 'PAID',
+          'TEXT': BX.message('PAYMENT_PAID_YES'),
+          'ONCLICK': BX.proxy(function () {
+            if (this.viewForm) {
+              this.showWindowPaidPayment();
+            } else {
+              var paymentPaid = BX('PAYMENT_PAID_' + indexes[k]);
+              if (paymentPaid)
+                paymentPaid.value = 'Y';
+
+              var isReturnChanged = BX('PAYMENT_IS_RETURN_CHANGED_' + indexes[k]);
+              if (isReturnChanged) {
+                isReturnChanged.value = 'N';
+              }
+
+              this.changePaidStatus('YES');
+
+              var obOperation = BX('OPERATION_ID_' + this.index);
+              if (obOperation)
+                obOperation.options[obOperation.selectedIndex].value = '';
+
+              for (var i in generalStatusFields) {
+                if (!generalStatusFields.hasOwnProperty(i))
+                  continue;
+                BX.style(generalStatusFields[i], 'display', 'none');
+              }
+              for (var i in returnStatusFields) {
+                if (!returnStatusFields.hasOwnProperty(i))
+                  continue;
+                BX.style(returnStatusFields[i], 'display', 'none');
+              }
+            }
+          }, this),
+        },
+      );
+    }
+
+    for (var k in indexes) {
+      if (!indexes.hasOwnProperty(k))
+        continue;
+      var act = new BX.COpener({
+        DIV: BX('BUTTON_PAID_' + indexes[k]).parentNode,
+        MENU: menu,
+      });
+    }
+  };
+
+  document.addEventListener('DOMContentLoaded', () => {
+    console.log('Test');
+    const paymentSelect = document.querySelector('[name="PAYMENT[1][PAY_SYSTEM_ID]"]');
+
+    if (paymentSelect) {
+      paymentSelect.disabled = true;
+    }
+  });
+</script>
