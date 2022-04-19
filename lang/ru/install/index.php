6c1a020cfe60c4f5716cf4d24dce0b87d5d87683
Mirlan Jelamanov <mirlan.jelamanov@gmail.com>
2022-04-19 12:01:54 +0600

    Fix typo in translation

--- lang/ru/install/index.php
+++ lang/ru/install/index.php
@@ -1,7 +1,7 @@
 <?php
 
-$MESS["BNPL_PAYMENT_INSTALL_NAME"] = "Модуль отплаты BNPL";
-$MESS["BNPL_PAYMENT_INSTALL_DESCRIPTION"] = "Модуль отплаты BNPL - «Купи сейчас, плати потом»";
+$MESS["BNPL_PAYMENT_INSTALL_NAME"] = "Модуль оплаты BNPL";
+$MESS["BNPL_PAYMENT_INSTALL_DESCRIPTION"] = "Модуль оплаты BNPL - «Купи сейчас, плати потом»";
 $MESS["BNPL_PAYMENT_INSTALL_TITLE"] = "Установка модуля";
 $MESS["BNPL_PAYMENT_UNINSTALL_TITLE"] = "Удаление модуля";
 $MESS["BNPL_PAYMENT_DEVELOPMENT_TEAM"] = "Команда разработчиков BNPL";
