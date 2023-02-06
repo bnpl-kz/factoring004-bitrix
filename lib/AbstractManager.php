e67ab8f22ee8104cf10cc3d34f1d06dd8d82afe3
Ivan Rybintsev <ivan.rybintsev@gmail.com>
2023-02-06 09:53:07 +0600

    Move findOrderPayment method to AbstractManager.php

--- lib/AbstractManager.php
+++ lib/AbstractManager.php
@@ -24,4 +24,18 @@ abstract class AbstractManager
         $this->order = $order;
         $this->items = $items;
     }
+
+    protected function findOrderPayment(): \Bitrix\Sale\Payment
+    {
+        /** @var \Bitrix\Sale\Payment $payment */
+        foreach ($this->order->getPaymentCollection() as $payment) {
+            $paySystemService = $payment->getPaySystem();
+
+            if ($paySystemService->getField('CODE') === 'factoring004') {
+                return $payment;
+            }
+        }
+
+        throw new InvalidArgumentException('Payment by code factoring004 is not found');
+    }
 }
