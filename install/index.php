c9d8d8d92264a52889b796b6d6cbbd9c83b8099b
Mirlan Jelamanov <mirlan.jelamanov@gmail.com>
2023-02-14 17:40:18 +0600

    Removes refund feature

--- install/index.php
+++ install/index.php
@@ -57,20 +57,6 @@ class bnpl_payment extends CModule
         );
 
         CopyDirFiles(
-            $_SERVER['DOCUMENT_ROOT'] . '/bitrix/modules/' . $this->MODULE_ID . '/install/actions/bnplpayment_return.php',
-            $_SERVER['DOCUMENT_ROOT'] . '/bitrix/admin/bnplpayment_return.php',
-            true,
-            true
-        );
-
-        CopyDirFiles(
-            $_SERVER['DOCUMENT_ROOT'] . '/bitrix/modules/' . $this->MODULE_ID . '/install/actions/bnplpayment_return_check_otp.php',
-            $_SERVER['DOCUMENT_ROOT'] . '/bitrix/admin/bnplpayment_return_check_otp.php',
-            true,
-            true
-        );
-
-        CopyDirFiles(
             $_SERVER['DOCUMENT_ROOT'] . '/bitrix/modules/' . $this->MODULE_ID . '/install/template/admin_header.php',
             $_SERVER['DOCUMENT_ROOT'] . '/bitrix/php_interface/admin_header.php',
             true,
@@ -113,8 +99,6 @@ class bnpl_payment extends CModule
     {
        DeleteDirFilesEx('/bitrix/php_interface/include/sale_payment/bnplpayment');
        DeleteDirFilesEx('/personal/order/payment/bnplpayment.php');
-       DeleteDirFilesEx('/bitrix/admin/bnplpayment_return.php');
-       DeleteDirFilesEx('/bitrix/admin/bnplpayment_return_check_otp.php');
        DeleteDirFilesEx('/bitrix/php_interface/admin_header.php');
        DeleteDirFilesEx('/bitrix/images/sale/sale_payments/bnplpayment.png');
        DeleteDirFilesEx('/bitrix/tmp/factoring004');
--- template/admin_header.php
+++ template/admin_header.php
@@ -1,48 +1,6 @@
 <?php
 
-require_once __DIR__ . '/../lib/Config.php';
-
-use Bitrix\Main\ArgumentNullException;
-use Bitrix\Sale\Order;
-use Bnpl\Payment\Config;
-
-if (strpos($_SERVER['REQUEST_URI'], 'admin/sale_order_view.php') !== false) {
-    if (empty($_GET['ID'])) {
-        return;
-    }
-
-    try {
-        $__order = Order::load($_GET['ID']);
-
-        if ((int) $__order->getField('PAY_SYSTEM_ID') !== (int) Config::getPaySystemId()) {
-            return;
-        }
-
-        if ($__order->isPaid()) {
-            require_once __DIR__ . '/disable_payment_status.php';
-            require_once __DIR__ . '/return_payment.php';
-        }
-    } catch (ArgumentNullException $e) {
-        // do nothing
-    }
-} elseif (strpos($_SERVER['REQUEST_URI'], 'admin/sale_order_payment_edit.php') !== false) {
-    try {
-        $__order = Order::load($_GET['order_id']);
-
-        if ((int) $__order->getField('PAY_SYSTEM_ID') !== (int) Config::getPaySystemId()) {
-            return;
-        }
-
-        if ($__order->isPaid()) {
-            \Bitrix\Main\UI\Extension::load('ui.notification');
-            require_once __DIR__ . '/disable_payment_status.php';
-            require_once __DIR__ . '/form_return_payment.php';
-        }
-    } catch (ArgumentNullException $e) {
-        // do nothing
-        return;
-    }
-} elseif (strpos($_SERVER['REQUEST_URI'], 'admin/sale_pay_system_edit.php') !== false) {
+if (strpos($_SERVER['REQUEST_URI'], 'admin/sale_pay_system_edit.php') !== false) {
     require_once __DIR__ . '/set_values_default.php';
     require_once __DIR__ . '/change_token_fields_type.php';
 }
