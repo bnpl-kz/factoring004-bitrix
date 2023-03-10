1500368ea0dc807968340383fcd284c79dc19823
Mirlan Jelamanov <mirlan.jelamanov@gmail.com>
2023-03-10 09:12:16 +0600

    Removes token fields modification

--- template/admin_header.php
+++ template/admin_header.php
@@ -2,5 +2,4 @@
 
 if (strpos($_SERVER['REQUEST_URI'], 'admin/sale_pay_system_edit.php') !== false) {
     require_once __DIR__ . '/set_values_default.php';
-    require_once __DIR__ . '/change_token_fields_type.php';
 }
