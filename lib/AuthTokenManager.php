8e484260357f1e1f6407ff85156cbbe795576098
Ivan Rybintsev <ivan.rybintsev@gmail.com>
2023-05-18 15:09:51 +0600

    Refactoring AuthTokenManager

--- lib/AuthTokenManager.php
+++ lib/AuthTokenManager.php
@@ -4,23 +4,26 @@ namespace Bnpl\Payment;
 
 use BnplPartners\Factoring004\OAuth\CacheOAuthTokenManager;
 use BnplPartners\Factoring004\OAuth\OAuthTokenManager;
+use BnplPartners\Factoring004\Transport\TransportInterface;
+use Bitrix\Main\Application;
 
 class AuthTokenManager
 {
     protected $manager;
-    public function __construct(string $login, string $password, string $apiHost, $transport, $instance)
+
+    public function __construct(string $login, string $password, string $apiHost, TransportInterface $transport, Application $instance)
     {
         $cache = new BitrixSimpleCache($instance->getCache());
         $tokenManager = new OAuthTokenManager($apiHost . '/users/api/v1', $login, $password, $transport);
         $this->manager = new CacheOAuthTokenManager($tokenManager, $cache, 'bnpl.payment');
     }
 
-    public static function init(string $login, string $password, string $apiHost, $transport, $instance): AuthTokenManager
+    public static function init(string $login, string $password, string $apiHost, TransportInterface $transport, Application $instance): AuthTokenManager
     {
         return new self($login, $password, $apiHost, $transport, $instance);
     }
 
-    public function getToken()
+    public function getToken(): string
     {
         return $this->manager->getAccessToken()->getAccess();
     }
