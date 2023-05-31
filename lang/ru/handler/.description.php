e48bef3fa2111861e19a6f2d6b89161a3cd2ade1
Zhandos <sutemgenov.zhandos@gmail.com>
2023-05-31 12:33:40 +0600

    adds preapp debug features

--- lang/ru/handler/.description.php
+++ lang/ru/handler/.description.php
@@ -4,3 +4,7 @@ $MESS["BNPL_PAYMENT_NAME"] = 'Рассрочка 0-0-4';
 $MESS["BNPL_PAYMENT_CLIENT_ROUTE"] = 'Вид интерфейса клиентского пути';
 $MESS["BNPL_PAYMENT_CLIENT_ROUTE_REDIRECT"] = 'Редирект';
 $MESS["BNPL_PAYMENT_CLIENT_ROUTE_MODAL"] = 'Модальное окно';
+$MESS["BNPL_PAYMENT_DEBUG_NAME"] = 'Показ ошибок';
+$MESS["BNPL_PAYMENT_DEBUG_DESCRIPTION"] = 'Не рекомендовано включать в продовой среде!';
+$MESS["BNPL_PAYMENT_DEBUG_ON"] = 'Включено';
+$MESS["BNPL_PAYMENT_DEBUG_OFF"] = 'Отключено';
