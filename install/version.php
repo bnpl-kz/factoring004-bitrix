c21054294da2ba5e56e089ed6a41f4f2ac38cd0e
Ivan Rybintsev <ivan.rybintsev@gmail.com>
2024-08-01 16:13:20 +0500

    Updated payment method description set logic

--- install/version.php
+++ install/version.php
@@ -1,6 +1,6 @@
 <?php
 
 $arModuleVersion = [
-    "VERSION" => "1.2.0",
-    "VERSION_DATE" => "2022-11-01",
+    "VERSION" => "1.2.1",
+    "VERSION_DATE" => "2024-08-01",
 ];
--- template/set_values_default.php
+++ template/set_values_default.php
@@ -9,15 +9,23 @@
         })
 
         function setDefaultValue(elem) {
-            let description = document.querySelector('iframe').contentWindow.document;
+            let descriptionEditor = document.querySelector('iframe').contentWindow.document;
+            let description = document.querySelector("input[name='DESCRIPTION']");
             if (elem.value === 'bnplpayment') {
                 document.querySelector("[name='CODE']").readOnly = true
                 document.querySelector("[name='CODE']").value = 'factoring004'
-                description.body.innerText = '<?= \Bitrix\Main\Localization\Loc::getMessage("BNPL_PAYMENT_DESCRIPTION"); ?>';
+                if (description.value.length == 0) {
+                    descriptionEditor.body.innerText = '<?= \Bitrix\Main\Localization\Loc::getMessage("BNPL_PAYMENT_DESCRIPTION"); ?>';
+                    description.value = '<?= \Bitrix\Main\Localization\Loc::getMessage("BNPL_PAYMENT_DESCRIPTION"); ?>';
+                }
             } else {
                 document.querySelector("[name='CODE']").readOnly = false
                 document.querySelector("[name='CODE']").value = ''
-                description.body.innerText = '';
+                id = document.querySelector("input[name='ID']")
+                if (id.value == 0) {
+                    descriptionEditor.body.innerText = '';
+                    description.value = '';
+                }
             }
         }
     })
